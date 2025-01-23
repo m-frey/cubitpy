2537311d3c6a7902680d57f532f0da18dfa9b9e2
Max Firmbach <max.firmbach@unibw.de>
2025-01-23 14:29:55 +0100

    Add testing

--- tests/testing.py
+++ tests/testing.py
@@ -771,6 +771,23 @@ def test_fluid_functionality():
     compare(cubit)
 
 
+def test_thermo_functionality():
+    """Test thermo conditions and thermo mesh creation."""
+
+    cubit = CubitPy()
+    thermo = create_brick(
+        cubit,
+        1,
+        1,
+        1,
+        mesh_interval=[1, 1, 1],
+        element_type=cupy.element_type.hex8_thermo,
+    )
+
+    # Compare the input file created for 4C.
+    compare(cubit)
+
+
 def test_fsi_functionality():
     """Test fsi and ale conditions and fluid mesh creation."""
 
